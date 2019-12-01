84757298395458590fc70fd3c4672d716cd47d92
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2019-12-01 17:00:05 -0500

    date picker

---
+++ app/src/main/java/com/example/midterm/CRACustomer.java
@@ -0,0 +1,18 @@
+package com.example.midterm;
+
+import android.util.Log;
+
+public class CRACustomer {
+
+    public String fNAme ;
+    public String lName;
+    String dob;
+
+    public  String fullName()
+    {
+        String Fullname = fNAme+" "+lName;
+        return  Fullname;
+
+    }
+
+}
--- app/src/main/java/com/example/midterm/MainActivity.java
+++ app/src/main/java/com/example/midterm/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.midterm;
 
 
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,12 +24,19 @@ import com.example.midterm.R;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText etdate;
     private TextView tvdate;
+
+    EditText fname;
+    EditText Lname;
+    Button btnshow;
+
+
     DatePickerDialog.OnDateSetListener setListener;
+    CRACustomer craCustomer = new CRACustomer();
 
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,28 +47,19 @@ public class MainActivity extends AppCompatActivity {
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        btnshow= findViewById(R.id.btnShow);
+        tvdate= findViewById(R.id.tv_date);
+        fname = findViewById(R.id.firstName);
+        Lname = findViewById(R.id.lastName);
 
-        tvdate = findViewById(R.id.tv_date);
+       craCustomer.fNAme = fname.getText().toString();
+       craCustomer.lName = Lname.getText().toString();
+       craCustomer.dob = etdate.getText().toString();
+//       craCustomer.fullName();
+        btnshow.setOnClickListener(this);
 
-        tvdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatePickerDialog datePickerDialog= new DatePickerDialog(MainActivity.this,R.style.DatePickerTheme,setListener,year,month,day);
 
-                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                datePickerDialog.show();
 
-            }
-        });
-        setListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month = month +1;
-                String date = day+ "/"+month+"/"+year;
-                tvdate.setText(date);
-
-            }
-        };
         etdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,6 +76,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        System.out.println(craCustomer.fullName());
     }
 }
 
